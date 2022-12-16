2053674b7c0f890a3132fde6cef1cb7c325ff7f3
taroy5913 <taroy5913@gmail.com>
2022-12-16 16:57:27 +0900

    #5 Modify layouts.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,17 +1,31 @@
-import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
+import { AppBar, Container, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography } from "@mui/material"
+import App from "./App";
 
-const App = () => {
+const Layout = () => {
     const theme = createTheme({
         typography: {
             fontSize: 12
         },
         palette: {
-            mode: "light"
+            mode: "dark"
         }
     });
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
+            <AppBar
+                position="absolute"
+                color="default"
+                elevation={0}
+                sx={{
+                    position: "relative",
+                    borderBottom: (t) => `1px solid $(t.palette.divider)`
+                }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" color="inherit">青空探索</Typography>
+                </Toolbar>
+            </AppBar>
             <Container component="main" maxWidth="sm" sx={{ mb: 4}}>
                 <App />
             </Container>
@@ -19,4 +33,4 @@ const App = () => {
     );
 }
 
-export default App
+export default Layout;

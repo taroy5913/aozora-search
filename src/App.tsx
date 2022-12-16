7015a4555d4658c3f5981c52097d565e4fc21898
taroy5913 <taroy5913@gmail.com>
2022-12-16 15:47:17 +0900

    #1 Show novels

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import { Button } from "@mui/material"
+import { Button, Container, createTheme, CssBaseline, Grid, Paper, ThemeProvider, Typography } from "@mui/material"
+import Novel from "./interfaces/Novel"
+import rawNovels from "./assets/sample_novels.json"
+import React from "react";
 
-function App() {
+const App = () => {
+    const theme = createTheme({
+        typography: {
+            fontSize: 12
+        },
+        palette: {
+            mode: "light"
+        }
+    });
+    const [novels, setNovels] = React.useState<Novel[]>(rawNovels as Novel[]);
+    React.useEffect(() => {
+        fetch("./assets/sample_novels.json")
+            .then(res => res.json())
+            .then(newNovels => setNovels(newNovels));
+    }, []);
     return (
-        <div>
-            <Button variant="contained">Hello World</Button>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container component="main" maxWidth="sm" sx={{ mb: 4}}>
+            {novels.map(novel => {
+                return (
+                    <Paper variant="outlined" sx={{ my: {xs: 3, md: 6}, p: {xs: 1, md: 1}}}>
+                        <Grid container>
+                            <Grid item xs={6} >
+                                <Typography variant="subtitle1" color="primary">{novel.title}</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography variant="subtitle1" align="right">{novel.author}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography variant="caption">
+                                    {novel.text.substring(0, 120) + "..."}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                );
+            })}
+            </Container>
+        </ThemeProvider>
     )
 }
 
---
+++ src/interfaces/Novel.ts
@@ -0,0 +1,10 @@
+export default interface Novel {
+    id: string;
+    card_id: string;
+    author: string;
+    translator: string;
+    title: string;
+    text: string;
+    info: string;
+    after_text: string;
+}
